bdd32763c7f02d2840dbd5cdc2b24078c876e26a
Gleb Fedchenko <glebf@tallium.com>
2020-01-20 21:39:20 +0200

    env vars

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,16 +3,36 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.REACT_APP_API_KEY,
+  authDomain:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.REACT_APP_DATABASE_URL,
   projectId: "ecom-d222f",
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.MEASURMENT_ID
+  storageBucket:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.REACT_APP_APP_ID,
+  measurementId:
+    (process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "production") &&
+    process.env.MEASURMENT_ID
 };
-
 export const createUserProfileDocument = async (
   userAuthObject,
   additionalData

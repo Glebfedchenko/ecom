0fa53fe6bfdaaccdcfea8a8d016435f6562a2335
Gleb Fedchenko <glebf@tallium.com>
2020-03-01 00:16:15 +0200

    added simple backend server

--- client/src/components/pieces/StipeButton/index.js
+++ client/src/components/pieces/StipeButton/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const pk_key = `pk_test_9UUs2MjkP67c9Ip75mo3kzdV00k3KQgUp0`;
-  const onToken = token => console.log(token);
+  const onToken = token => {
+    axios({
+      url: "/payment",
+      method: "post",
+      data: { amount: priceForStripe, token }
+    })
+      .then(res => {
+        alert("Payment Successfull");
+      })
+      .catch(error => {
+        alert("Payment Error");
+      });
+    console.log(token);
+  };
   //const FAKE_CARD = `4242-4242-4242-4242 exp 01/20 cw 123`;
   return (
     <StripeCheckout
